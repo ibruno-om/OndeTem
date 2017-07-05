055b11a7c0143d62c4cf1c3b7759f31a143959b6
Unknown <ibruno.om@gmail.com>
2017-07-05 00:13:31 -0300

    Add Firebase connection

--- app/src/main/java/br/ufg/inf/dsdm/ondetem/Home.java
+++ app/src/main/java/br/ufg/inf/dsdm/ondetem/Home.java
@@ -2,12 +2,23 @@ package br.ufg.inf.dsdm.ondetem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Home extends AppCompatActivity {
+
+    private Button btnSendData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 }
