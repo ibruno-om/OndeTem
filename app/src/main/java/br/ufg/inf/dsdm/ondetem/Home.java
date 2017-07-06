7f84b0eefb9a3a58b6efcbbfb44684b93f48ddef
Unknown <ibruno.om@gmail.com>
2017-07-05 23:38:24 -0300

    Adição de entitdade para pergunta e listagem na home

--- app/src/main/java/br/ufg/inf/dsdm/ondetem/Home.java
+++ app/src/main/java/br/ufg/inf/dsdm/ondetem/Home.java
@@ -1,24 +1,110 @@
 package br.ufg.inf.dsdm.ondetem;
 
+import android.content.pm.PermissionGroupInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.ufg.inf.dsdm.ondetem.model.Pergunta;
 
 public class Home extends AppCompatActivity {
 
-    private Button btnSendData;
+    private ListView questionList;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private List<Pergunta> perguntas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        questionList = (ListView) findViewById(R.id.questionList);
 
-        myRef.setValue("Hello, World!");
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.signInWithEmailAndPassword("ibruno.om@gmail.com", "senha321");
+
+        perguntas = new ArrayList<Pergunta>();
+
+        perguntas.add(new Pergunta());
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(FirebaseAuth firebaseAuth) {
+
+                user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference reference = database.getReference();
+
+                    reference.child("user").child(user.getUid())
+                            .child("pergunta").addValueEventListener(new ValueEventListener() {
+
+
+
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+                            perguntas.clear();
+
+                            for (DataSnapshot child : children) {
+                                Pergunta pergunta = child.getValue(Pergunta.class);
+                                perguntas.add(pergunta);
+                            }
+
+                            ArrayAdapter adapter = new ArrayAdapter<Pergunta>(Home.this,
+                                    android.R.layout.simple_list_item_1, perguntas);
+
+                            questionList.setAdapter(adapter);
+
+
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+                    Log.d("LISTA", "Deu certo");
+                }
+
+            }
+        };
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
