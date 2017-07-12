f8123f4a946ec532ee5b96acfc330ee7f1a160a1
Unknown <ibruno.om@gmail.com>
2017-07-11 23:42:29 -0300

    Adição de minhas perguntas

--- app/src/main/java/br/ufg/inf/dsdm/ondetem/MyQuestionActivity.java
+++ app/src/main/java/br/ufg/inf/dsdm/ondetem/MyQuestionActivity.java
@@ -1,7 +1,11 @@
 package br.ufg.inf.dsdm.ondetem;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.ufg.inf.dsdm.ondetem.fragment.MyQuestionFragment;
 
 public class MyQuestionActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MyQuestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_question);
+
+        MyQuestionFragment questionFragment = new MyQuestionFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ftransaction = fragmentManager.beginTransaction();
+        ftransaction.replace(R.id.myQuestionListFrame, questionFragment);
+        ftransaction.commit();
     }
 }
